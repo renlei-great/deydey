99b5bac8b5a8b1cb7195b0fd3872d609ff46d6f6
renlei-great <1415977534@qq.com>
2019-12-19 20:00:20 +0800

    版本v1.1.1-ubuntu-注册页面-进行发送163邮件进行激活会员帐号

--- daliyfresh/apps/user/urls.py
+++ daliyfresh/apps/user/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import include, url
 
-from user.views import RegisterView
+from user.views import *
 
 urlpatterns = [
     url(r'^register$', RegisterView.as_view(), name='register'),  # 注册
+    url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 激活
+    url(r'^login$', LoginView.as_view(), name='login'),  # 登录
+
 ]
--- daliyfresh/apps/user/views.py
+++ daliyfresh/apps/user/views.py
@@ -2,59 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from user.models import User
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired
 from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
+from daliyfresh import settings
 import re
 
 # Create your views here.
 
-"""def register(request):
-
-    return render(request, 'register.html')
-
-
-def register_handle(request):
-    # 接受数据
-    username = request.POST.get('user_name')  # 用户名
-    pwd = request.POST.get('pwd')  # 密码
-    cpwd = request.POST.get('cpwd')  # 确认密码
-    email = request.POST.get('email')  # 邮箱
-    allow = request.POST.get('allow')  # 是否同意条款
-    # 验证数据
-    if not all([username, pwd, cpwd, email, allow]):
-        return render(request, 'register.html', {'errmsg': '数据填写不完整'})
-
-    # 验证用户名是否被注册过
-    try:
-        user = User.objects.get(username = username)
-    except User.DoesNotExist:
-        user = None
-    if user:
-        # 用户名已经被注册
-        return render(request, 'register.html', {'errmsg': '用户名已经被注册'})
-
-    # 验证邮箱是否填写正确
-    if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-        return render(request, 'register.html', {'errmsg': '邮箱填写不正确'})
-
-    # 判断两次密码是否一致
-    if not pwd == cpwd:
-        return render(request, 'register.html', {'errmsg': '两次密码不一致'})
-
-    # 判断是否同意条款
-    # if allow != 'no':
-    #     return render(request, 'register.html')
-
-    # 进行业务处理：保存数据
-    user = User.objects.create_user(username, email, pwd)
-    user.is_active = 0
-    user.save()
-
-    # 重定向
-    return redirect(reverse('goods:index'))"""
-
 
 class RegisterView(View):
-
+    """注册的两种提交方式"""
     def get(self, request):
         return render(request, 'register.html')
 
@@ -66,7 +25,7 @@ class RegisterView(View):
         email = request.POST.get('email')  # 邮箱
         allow = request.POST.get('allow')  # 是否同意条款
         # 验证数据
-        if not all([username, pwd, cpwd, email, allow]):
+        if not all([username, pwd, cpwd, email]):
             return render(request, 'register.html', {'errmsg': '数据填写不完整'})
 
         # 验证用户名是否被注册过
@@ -87,13 +46,60 @@ class RegisterView(View):
             return render(request, 'register.html', {'errmsg': '两次密码不一致'})
 
         # 判断是否同意条款
-        # if allow != 'no':
-        #     return render(request, 'register.html')
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意条款'})
 
         # 进行业务处理：保存数据
         user = User.objects.create_user(username, email, pwd)
         user.is_active = 0
         user.save()
 
+        # 对id进行加密
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        info = {'user_id':user.id}
+        res = serializer.dumps(info)
+        # 对res转换字符串
+        res = res.decode()  # 如果不对其进行utf8解码会出现错误，将二进制转为字符串
+        # 发送邮件链接，包含激活练级：192.168.223.128:7788/user/active/1
+        # 组织邮件发送内容
+        # 主题
+        subject = '注册激活'
+        # html内容
+        message = ''
+        html_mag = '<h1>%s 欢迎您成为天天生鲜会员</h1><br>请点击下方链接激活会员<br><a href = "http://192.168.223.128:7788/user/active/%s">http://192.168.223.128:7788/user/active/%s </a>' %(username,res,res)
+        # 收件人列表
+        receiver = [email]
+        # 发件人
+        sender = settings.EMAIL_FROM
+
+        # 发送
+        send_mail(subject, message, sender, receiver , html_message=html_mag)
         # 重定向
         return redirect(reverse('goods:index'))
+
+
+# user/active/1
+class ActiveView(View):
+
+    def get(self, request, token):
+        """激活用户"""
+        # 对token进行解密
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        try:
+            tokens = serializer.loads(token)
+        except SignatureExpired:
+            return HttpResponse('验证过期')
+        # 向数据库中写入数据
+        user = User.objects.get(id = tokens['user_id'])
+        user.is_active = 1
+        user.save()
+
+        # 返回登录界面
+        return redirect(reverse('user:login'))
+
+
+class LoginView(View):
+    """登录界面"""
+    def get(self,request):
+        return render(request, 'login.html')
+
